f4158ef5dd03acc3909f82045c6e1828322e0d65
MariiaNk <mariya.nyzhnyk2006@gmail.com>
2024-10-10 12:50:52 +0200

    Fix output, change to output 40 layers and cycle from 1 to n-1(80)

--- Wprowadzenie do informatyki/AutomatKomorkowy.py
+++ Wprowadzenie do informatyki/AutomatKomorkowy.py
@@ -2,22 +2,20 @@
 
 def print_array(arr, n):
     for i in range(n):
-        print(arr[i], end = '')
-    print('\n')
-    
+        print(arr[i], end='')
+    print()
+
 n = 81
-prev_arr = [' ']*(n+1)
-curr_arr = [' ']*(n+1)
+prev_arr = [' '] * (n + 1)
+curr_arr = [' '] * (n + 1)
 
-curr_arr[n//2]='o';
+curr_arr[n // 2] = 'o'
 
-for i in range (1, n//2):
+for _ in range(40):  
     print_array(curr_arr, n)
     prev_arr = curr_arr[:]
-    for j in range (2, n-1):
-        if prev_arr[j-1] == prev_arr[j+1]:
+    for j in range(1, n - 1):
+        if prev_arr[j - 1] == prev_arr[j + 1]:
             curr_arr[j] = ' '
         else:
             curr_arr[j] = 'o'
-        #print(prev_arr)
-        #print(j, prev_arr[j-1], prev_arr[j+1], curr_arr[j])
