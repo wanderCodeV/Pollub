99ef6b2730824e0d50833836e929377d85ac4ec6
MariiaNk <mariya.nyzhnyk2006@gmail.com>
2024-10-19 19:09:33 +0200

    Add situation remis in game paper, rock, scisors

--- Wprowadzenie do informatyki/Podstawowe operacje na danych. Interakcja z użytkownikiem. Instrukcja warunkowa/Task1.py
+++ Wprowadzenie do informatyki/Podstawowe operacje na danych. Interakcja z użytkownikiem. Instrukcja warunkowa/Task1.py
@@ -4,14 +4,17 @@ player2 = int(input("Gracz 2: Podaj symbol (0 – papier, 1 – nożyce, 2 – k
 if player1 < 0 or player2 < 0 or player1 > 2 or player2 > 2:
     print("Nieprawidlowe dane wejsciowe")
 else:
-    result = player2 - player1
-    if abs(result) == 2:
-        result *= -1
-
-    if result < 0:
-        print("Wygral gracz 1")
+    if player1 == player2 :
+        print("Remis!")
     else:
-        print("Wygral gracz 2")
+        result = player2 - player1
+        if abs(result) == 2:
+            result *= -1
+
+        if result < 0:
+            print("Wygral gracz 1")
+        else:
+            print("Wygral gracz 2")
 
 
 # nożyce > papier 1 > 0
