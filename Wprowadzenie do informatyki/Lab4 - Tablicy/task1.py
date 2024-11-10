35ca57352548d302c4a3a6069068c3bd06197035
MariiaNk <mariya.nyzhnyk2006@gmail.com>
2024-11-10 12:19:35 +0100

    Detail the output, comment for the biggest number and second biggest number

--- Wprowadzenie do informatyki/Lab4 - Tablicy/task1.py
+++ Wprowadzenie do informatyki/Lab4 - Tablicy/task1.py
@@ -1,11 +1,12 @@
-n = int(input('Podaj dlugosc tablicy: '))
-tab = [0]*n
+n = int(input("Podaj dlugosc tablicy: "))
+tab = [0] * n
 i = 0
 
-for i in range (n):
-    tab[i] = int(input('Podaj kolejna liczbe: '))
+for i in range(n):
+    tab[i] = int(input("Podaj kolejna liczbe: "))
 
 print(tab)
 
 tab.sort()
-print(tab[n-1], tab[n-2])
+print("Pierwsza najwieksza ", tab[n - 1])
+print("Druga najwieksza ", tab[n - 2])
